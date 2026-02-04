420968032bbf73d2a4dbca59d29a889eed72b093
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2026-02-04 18:38:31 +0200

    Add vite for frontend

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { topSearches, addSearch } from '../controllers/searcheController.js';
+import { topSearches, addSearch } from '../controllers/searchController.js';
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../', 'public')));
 app.use(express.json());
 app.use(cors());
 
