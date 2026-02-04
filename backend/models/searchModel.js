64a2279fd7fe9464ca2adbd659aeaae49fe2a31d
Valdemaras Sinkevicius <valdemarassinkevicius@gmail.com>
2026-02-04 14:28:02 +0200

    Add schema

---
+++ backend/models/searchModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const searchSchema = new Schema({
+  selectedCity: {
+    type: String,
+    required: true,
+  },
+  searchDate: {
+    type: Date,
+    default: Date.now,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export default mongoose.model('Search', searchSchema);
